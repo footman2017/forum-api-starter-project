eddd3b13af0c90e86875e210588cedd1c91a7384
Raey Faldo <raey3221@gmail.com>
2024-11-14 11:29:36 +0700

    add thread repository

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,8 @@
+class ThreadRepository {
+    async createThread(userId, newThread) {
+      throw new Error("THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
+  }
+  
+  module.exports = ThreadRepository;
+  
